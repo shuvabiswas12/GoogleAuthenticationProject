668b3c98b57e7d0ee8e2278cefc3443ef030c330
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-07-25 11:16:22 +0600

    google login done.

--- GoogleAuthenticationProject.App/Controllers/HomeController.cs
+++ GoogleAuthenticationProject.App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GoogleAuthenticationProject.App.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace GoogleAuthenticationProject.App.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
--- GoogleAuthenticationProject.App/Program.cs
+++ GoogleAuthenticationProject.App/Program.cs
@@ -1,7 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GoogleAuthenticationProject.App.Data;
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// adding external google authentication to the services.
+// without adding this google login will not be worked.
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
+
+var connectionString = configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+// must be given so that return url works properly
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = "/Identity/Account/Logout";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
 
 var app = builder.Build();
 
@@ -17,8 +48,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
